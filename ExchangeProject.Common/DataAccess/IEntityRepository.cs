6aa0d2a4f3e2c6eba71025ab1ef5133b1ee20260
yavuz.emilli <yavuzemilli@gmail.com>
2021-09-02 17:51:49 +0300

    Added unit of work and generic repository

---
+++ ExchangeProject.Common/DataAccess/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using ExchangeProject.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeProject.Common.DataAccess
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate);
+        Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ ExchangeProject.DataAccess/Abstract/IUow.cs
@@ -0,0 +1,27 @@
+﻿using ExchangeProject.Common.DataAccess.EntityFramework;
+using ExchangeProject.Common.Entities;
+using ExchangeProject.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeProject.Common.DataAccess
+{
+    public interface IUow : IAsyncDisposable
+    {
+        IAppRoleRepository AppRole { get; }
+        IAppUserRepository AppUser { get; }
+        IAssetCoinRepository AssetCoin { get; }
+        IAssetRepository Asset { get; }
+        IWalletRepository Wallet { get; }
+        ICoinRepository Coin { get; }
+        IPairRepository Pair { get; }
+        ICoinPairRepository CoinPair { get; }
+        IWalletTransactionRepository WalletTransaction { get; }
+        ITransactionRepository Transaction { get; }
+
+        Task<int> SaveAsync();
+    }
+}
